7f77ad3dee6549dbe503a30c68492a40f883f287
anusingh012403-design <anusingh012403@gmail.com>
2026-04-17 14:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Gait Analysis Dashboard")
-st.write("Welcome to my new dashboard")
+
+# Read CSV file
+df = pd.read_csv("Subject_ID 1.csv")
+
+# Show dataset
+st.subheader("Dataset Preview")
+st.write(df)
+
+# Show column names
+st.subheader("Parameters / Columns")
+st.write(df.columns)
+
+# Select column for chart
+column = st.selectbox("Choose Parameter", df.columns)
+
+st.subheader("Graph")
+st.line_chart(df[column])
